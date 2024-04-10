45cd6e929e267245d4b2c62a38ed14143713bf62
Brand0n25 <emmanuelbrandon328@gmail.com>
2024-04-10 19:54:34 +0700

    [initial commit]

---
+++ src/api/components/users/users-repository.js
@@ -0,0 +1,99 @@
+const { User } = require('../../../models');
+const { password } = require('../../../models/users-schema');
+
+/**
+ * Get a list of users
+ * @returns {Promise}
+ */
+async function getUsers() {
+  return User.find({});
+}
+
+/**
+ * Get user detail
+ * @param {string} id - User ID
+ * @returns {Promise}
+ */
+async function getUser(id) {
+  return User.findById(id);
+}
+
+/**
+ * Create new user
+ * @param {string} name - Name
+ * @param {string} email - Email
+ * @param {string} password - Hashed password
+ * @returns {Promise}
+ */
+async function createUser(name, email, password) {
+  return User.create({
+    name,
+    email,
+    password,
+  });
+}
+
+/**
+ * Update existing user
+ * @param {string} id - User ID
+ * @param {string} name - Name
+ * @param {string} email - Email
+ * @returns {Promise}
+ */
+async function updateUser(id, name, email) {
+  return User.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        name,
+        email,
+      },
+    }
+  );
+}
+
+
+
+
+/**
+ * find by email
+ * @param {string} email - Email
+ * @returns {Promise}
+ */
+async function findByEmail(email) {
+  return User.findOne({ email });
+}
+
+/**
+ * Delete a user
+ * @param {string} id - User ID
+ * @returns {Promise}
+ */
+async function deleteUser(id) {
+  return User.deleteOne({ _id: id });
+}
+
+async function changePassword(newPassword , id){
+  return User.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        password : newPassword,
+      },
+    }
+  );
+}
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  findByEmail,
+  changePassword
+};
---
+++ src/api/components/users/users-validator.js
@@ -0,0 +1,29 @@
+const joi = require('joi');
+
+module.exports = {
+  createUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+      password: joi.string().min(6).max(32).required().label('Password'),
+      confirmPassword : joi.string().min(6).max(32).required().label('Confirm Password')
+    },
+  },
+
+  updateUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+    },
+  },
+
+  changePassword: {
+    body: {
+      oldPassword: joi.string().min(6).max(32).required().label('Old Password'),
+      newPassword: joi.string().min(6).max(32).required().label('New Password'),
+      confirmPassword: joi.string().valid(joi.ref('newPassword'))
+        .required().label('Confirm New Password')
+        .messages({ 'any.only': '{{#label}} must match new password' }),
+    },
+  },
+};
